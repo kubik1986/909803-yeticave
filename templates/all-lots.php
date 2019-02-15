8f8c8e95abbfa90c799c9322329f0d6fd6e4ee90
Viacheslav Akulov <sentence@ngs.ru>
2019-02-15 10:29:31 +0700

    Update all-lots.php

--- templates/all-lots.php
+++ templates/all-lots.php
@@ -1,7 +1,5 @@
 <section class="lots">
-    <div class="lots__header">
-        <h2>Все лоты в категории «<?=$categories[$category_id - 1]['name']; ?>»</h2>
-    </div>
+    <h2>Все лоты в категории «<?=$categories[$category_id - 1]['name']; ?>»</h2>
     <?=$lots_list; ?>
 </section>
 <?php if ($pagination_data): ?>
